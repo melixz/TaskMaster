d718804a95ada7db9605f6bf743204ef6bd327b6
Vlad <dr.melix@gmail.com>
2024-07-26 23:07:18 +0300

    Настроил Celery и RabbitMQ для асинхронной обработки задач

---
+++ TaskMaster/celery.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TaskMaster.settings')
+
+app = Celery('TaskMaster')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
---
+++ TaskMaster/tasks/tasks.py
@@ -0,0 +1,11 @@
+from celery import shared_task
+from time import sleep
+from .models import Task
+
+
+@shared_task
+def process_task(task_id):
+    sleep(10)
+    task = Task.objects.get(id=task_id)
+    task.status = 'completed'
+    task.save()
